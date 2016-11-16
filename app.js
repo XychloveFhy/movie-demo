0864b054a3506a436e95fdae67d6f223956a0fdd
hanekaoru <vd2o@foxmail.com>
2016-11-16 17:05:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 3000;
 
-//更新时间
+// 更新时间
 app.locals.moment = require('moment');
 
 // 传入本地的数据库
@@ -36,13 +36,13 @@ console.log("Service is start , Port in :" + port);
 // index page 
 app.get('/', function (req, res) {
 
-	//直接调用模型，在回调方法中内拿到返回的数组movies
+	// 直接调用模型，在回调方法中内拿到返回的数组movies
 	Movie.fetch(function (err, movies) {
 		if (err) {
 			console.log(err);
 		}
 
-		//渲染首页，同时把查询到的电影列表（key）赋值给movies
+		// 渲染首页，同时把查询到的电影列表（key）赋值给movies
 		res.render('index', {
 			title: 'demo1 首页1',
 			movies: movies
@@ -50,10 +50,10 @@ app.get('/', function (req, res) {
 	});
 });
 
-//detail page 
+// detail page 
 app.get('/detail/:id', function (req, res) {
 
-	//拿到detail后的 :id
+	// 拿到detail后的 :id
 	var id = req.params.id;
 
 	// 拿到查询后的id数据，将详情页返回回去
@@ -66,7 +66,7 @@ app.get('/detail/:id', function (req, res) {
 	})
 });
 
-//admin page
+// admin page
 app.get('/admin/movie', function (req, res) {
 	res.render('admin', {
 		title: 'demo1 后台录入页',
@@ -85,7 +85,7 @@ app.get('/admin/movie', function (req, res) {
 });
 
 
-//admin update movie
+// admin update movie
 // 匹配到 update/:id，如果是从这个URL地址过来的，那么是需要更新这个地址
 app.get('/admin/update/:id', function (req, res) {
 
@@ -103,7 +103,7 @@ app.get('/admin/update/:id', function (req, res) {
 	}
 });
 
-//admin delete movie
+// admin delete movie
 app.delete('/admin/list', function (req, res) {
 	var id = req.query.id;
 	if (id) {
@@ -118,7 +118,7 @@ app.delete('/admin/list', function (req, res) {
 
 })
 
-//admin post movie 拿到从后台路由页post过来的数据
+// admin post movie 拿到从后台路由页post过来的数据
 app.post('/admin/movie/new', function (req, res) {
 
 	var id = req.body.movie._id;
@@ -127,7 +127,7 @@ app.post('/admin/movie/new', function (req, res) {
 
 	
 
-	//如果id不是undefined，说明这部电影已经在数据库存储过的，这时候就需要进行更新
+	// 如果id不是undefined，说明这部电影已经在数据库存储过的，这时候就需要进行更新
 	if (id !== undefined) {
 		Movie.findById(id, function (err, movie) {
 			if (err) {
@@ -139,13 +139,13 @@ app.post('/admin/movie/new', function (req, res) {
 			// extend方法 用另外一个对象里面新的字段替换掉老的里面对应的字段（查询的目标，新的字段放在第二个位置）
 			_movie = _.extend(movie, movieObj);
 	
-			//save后的movie
+			// save后的movie
 			_movie.save(function (err, movie) {
 				if (err) {
 					console.log(err);
 				}
 
-				// 数据更新了，并且存储成功了，就让页面重定向到这部电影的详情页面
+				// 数据更新了，并且存储成功了，就将页面重定向到这部电影的详情页面
 				res.redirect('/detail/' + movie._id);
 			});
 		});
@@ -153,7 +153,7 @@ app.post('/admin/movie/new', function (req, res) {
 	} else {	
 
 		
-		//如果这部电影的id是没有定义过的，也就是post表单内是没有movie._id这个值的，说明这个电影是新增加的
+		// 如果这部电影的id是没有定义过的，也就是post表单内是没有movie._id这个值，说明这个电影是新增加的
 		_movie = new Movie({
 			doctor: movieObj.doctor,
 			title: movieObj.title,
@@ -165,7 +165,7 @@ app.post('/admin/movie/new', function (req, res) {
 			summary: movieObj.summary
 		});
 
-		//同上，保存电影，同时重定向
+		// 同上，保存电影，同时重定向
 		_movie.save(function (err, movie) {
 			if (err) {
 				console.log(err);
@@ -177,7 +177,7 @@ app.post('/admin/movie/new', function (req, res) {
 
 
 
-//list page
+// list page
 app.get('/admin/list', function (req, res) {
 
 	// 和首页查询是一样的
